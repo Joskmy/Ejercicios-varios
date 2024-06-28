7e8247d6a102c1b00cd045969c4467ae8ba4f561
José Miguel Posada O <jmposadao22@gmail.com>
2024-06-27 22:56:27 -0500

    Actualización del orden de las carpetas

---
+++ EjerciciosJava/EjerciciosVariadosJava/src/ejercicios/basicos/computadoras/TestComputador.java
@@ -0,0 +1,15 @@
+package ejercicios.basicos.computadoras;
+
+public class TestComputador {
+	public static void main(String[] args) {
+		Computadora computadora1 = new Computadora("Computadora HP", new Monitor("HP", 13), 
+									new Teclado("bluetooth", "hp"), new Raton("bluetooth", "HP"));
+		
+		
+		
+		Orden orden1 = new Orden();
+		orden1.agregarComputadoras(computadora1);
+		
+		orden1.mostrarOrden();
+	}
+}
---
+++ EjerciciosJava/EjerciciosVariadosJava/src/ejercicios/basicos/ventas/Orden.java
@@ -0,0 +1,41 @@
+package ejercicios.basicos.ventas;
+
+public class Orden{
+	private int idOrden;
+	private Producto productos[];
+	private static int contadorOrdenes;
+	private int contadorProductos;
+	private static final int MAX_PRODUCTOS = 10;
+	
+	public Orden() {
+		this.idOrden = ++Orden.contadorOrdenes;
+		this.productos = new Producto[Orden.MAX_PRODUCTOS];
+	}
+	
+	public void agregarProducto(Producto producto) {
+		if( !(this.contadorProductos > Orden.MAX_PRODUCTOS)) {
+			System.out.println("Se ha superado el máximo de productos: " + Orden.MAX_PRODUCTOS);
+			return;
+		}
+		this.productos[this.contadorProductos++] = producto;
+	}
+	
+	public double calcularTotal() {
+		double total = 0;
+		
+		for (int i=0; i < this.contadorProductos; i++) {
+			total += this.productos[i].getPrecio();
+		}
+		
+		return total;
+	}
+	
+	public void mostrarOrden() {
+		System.out.println("Id orden: " + this.idOrden);
+		System.out.println("Total de la orden: " + this.calcularTotal());
+		System.out.println("Productos de la Orden: ");
+		for (int i = 0; i < this.contadorProductos; i++) {
+			System.out.println(this.productos[i]);
+		}
+	}
+}
